132973ff4a325bde96cd7ba43e85ed7e4b11dfe5
ranemihir <mihirrane171@gmail.com>
2021-10-04 10:41:40 +0530

    minor fixes

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -20,7 +20,7 @@ function emailMatcher(c: AbstractControl): { [key: string]: boolean; } | null {
 
 function rangeValidator(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean; } | null => {
-    if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
       return { 'range': true };
     }
 
